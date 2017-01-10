5b5cf8dadfa0f8ea4ead9c08421b35a6fddf5c76
Dylan Stein <djstein@ncsu.edu>
2017-01-10 02:25:15 -0500

    app structure

--- project/settings.py
+++ project/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'project',
     'webpack_loader',
+    'appname',
 )
 
 MIDDLEWARE_CLASSES = (
